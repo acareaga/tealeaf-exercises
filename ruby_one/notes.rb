4ec5624e83bfb7013be5e3bf6c4e5b1d17f8fe08
Aaron Careaga <aaroncareaga@gmail.com>
2015-08-11 16:06:09 -0600

    Lesson 2, chapter 16 & 17

--- ruby_one/notes.rb
+++ ruby_one/notes.rb
@@ -1,1 +1,2 @@
-# Prep work has been going well. I’m currently on TeaLeafs Ruby Foundations Lesson 2 number 15, about 25% through the Code Academy Ruby course, and just starting the Chris Pine tutorial book. Hopefully I’m on pace with the group. 
+# Prep work has been going well. I’m currently on TeaLeafs Ruby Foundations Lesson 2 number 15, about 25% through the Code Academy Ruby course, and just starting the Chris Pine tutorial book. Hopefully I’m on pace with the group.
+ 
