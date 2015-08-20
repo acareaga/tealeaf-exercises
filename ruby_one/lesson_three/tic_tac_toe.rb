85e822c31a398b87611aabad4cfc8fc3712cb053
Aaron Careaga <aaroncareaga@gmail.com>
2015-08-19 20:54:58 -0600

    Tic Tac Toe player turn added.

--- ruby_one/lesson_three/tic_tac_toe.rb
+++ ruby_one/lesson_three/tic_tac_toe.rb
@@ -1,4 +1,10 @@
-# Tic Tac Toe
+INITIAL_MARKER = ' '
+PLAYER_MARKER = 'X'
+COMPUTER_MARKER = 'O'
+
+def prompt(msg)
+  puts "=> #{msg}"
+end
 
 def display_board(brd)
   puts ""
@@ -18,9 +24,28 @@ end
 
 def intialize_board
   new_board = {}
-  (1..9).each { |num| new_board[num] = ' ' }
+  (1..9).each { |num| new_board[num] = INITIAL_MARKER }
   new_board
 end
 
+def empty_square(brd)
+  brd.keys.select {|num| brd[num] == INITIAL_MARKER}
+end
+
+def player_places_piece!(brd)
+  square = ''
+  loop do
+    prompt "Choose a square (#{empty_square(brd).join(', ')}):"
+    square = gets.chomp.to_i
+    break if empty_square(brd).include?(square)
+    prompt "Sorry, thats not a valid choice."
+  end
+  brd[square] = PLAYER_MARKER
+end
+
 board = intialize_board
+display_board(board)
+
+player_places_piece!(board)
+puts board.inspect
 display_board(board)
